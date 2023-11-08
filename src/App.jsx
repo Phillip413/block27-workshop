11cbdb5ce6ee01e2f6cff7390b4ba46f2029551e
Phillip Sim <phillipsim@phillips-air.mynetworksettings.com>
2023-11-08 14:48:14 -0500

    worked on authenticate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import SignUpForm from './Components/SignUpForm'
 import Authenticate from './Components/Authenticate'
 import './App.css'
+import { useState} from 'react'
+
 
 function App() {
-
+  
+  const [token, setToken] = useState(null)
 
   return (
     <>
-      <SignUpForm />
-      <Authenticate />
+      <SignUpForm token={token} setToken={setToken}/>
+      <Authenticate token={token} setToken={setToken}/>
     </>
   )
 }
--- src/Components/Authenticate.jsx
+++ src/Components/Authenticate.jsx
@@ -1,8 +1,19 @@
 
 function Authenticate () {
+setToken(result.token);
+return (
+  <>
+    <h2>Authenticate</h2>
+    
+    <button type="button" onClick={() => {
+      let response = await fetch ("https://fsa-jwt-practice.herokuapp.com/signup")
+      let json = await response.json()
+    }}>Authenticate Token</button>
+  
+  </>
 
-return <h2>Authenticate</h2>
-
+)
+  
 }
 
 export default Authenticate
